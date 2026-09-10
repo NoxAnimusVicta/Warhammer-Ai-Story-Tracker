2e506872520eba571ca57b6d1662048ae5fb5121
NoxAnimusVicta <NoxAnimusVicta@gmail.com>
2026-09-10 22:58:53 +1000

    Refine empyrean dossier design for iPhone

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 'use strict';
 const BASE=new URL('./',self.location.href);
 const PREFIX='malaspina-dossier:'+BASE.pathname+':';
-const CACHE=PREFIX+'v1';
+const CACHE=PREFIX+'v2';
 const FILES=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES.map(path=>new URL(path,BASE).href))).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith(PREFIX)&&key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
